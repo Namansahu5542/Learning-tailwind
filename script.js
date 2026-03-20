1beb180f09e5d0d799bdb6e39e327af6822b07f7
Naman Sahu <namansahu5542@gmail.com>
2026-03-20 18:19:39 +0530

    some changes

--- script.js
+++ script.js
@@ -95,10 +95,3 @@ btn.addEventListener("click", () => {
         placeholder.value = "";
     }
 });
-// echo "# Learning-tailwind" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/Namansahu5542/Learning-tailwind.git
-// git push -u origin main
